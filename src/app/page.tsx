42bb309e9c25ea10e6e12b36d9aa04d36bed9c21
Klaudjo Shkurta <shkurtaklaudjo@gmail.com>
2025-05-09 20:05:25 +0300

    Enhance RootLayout structure and update Home component to manage first and last name state with controlled inputs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+
 	return (
 		<div className="h-screen flex items-center">
 			<div className="w-full max-w-xl mx-auto px-4">
 				{/** Form */}
 				<div className="box-shadow rounded-xl flex flex-col">
-					<input className="p-4 rounded-xl z-10" type="text" name="" id="" placeholder="Enter your first name" />
+					<input className="p-4 rounded-xl z-10 outline-0" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="Enter your first name" />
 					<hr className="border-gray-200/80 z-0" />
-					<input className="p-4 rounded-xl z-10" type="text" name="" id="" placeholder="Enter your last name" />
+					<input className="p-4 rounded-xl z-10 outline-0" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Enter your last name" />
 				</div>
 			</div>
 		</div>
